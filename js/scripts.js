b5e43ed6d9070b0a04d288005c6ec55ebcfa3df5
Rajinder Dhillon <dhillon_rajinder@hotmail.com>
2019-03-16 17:19:25 -0500

    updates lat/long

--- js/scripts.js
+++ js/scripts.js
@@ -237,7 +237,7 @@ $(document).ready(function () {
 
 // Google map
 function initMap() {
-    var itc_kol = {lat: 44.871367, lng: -93.308125};
+    var itc_kol = {lat: 44.871367, lng: 93.308125};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: itc_kol,
@@ -251,7 +251,7 @@ function initMap() {
 }
 
 function initBBSRMap() {
-    var la_fiesta = {lat: 20.305826, lng: 85.85480189999998};
+    var la_fiesta = {lat: 44.871367, lng: 93.308125};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: la_fiesta,
